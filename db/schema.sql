f482595b77d252d21e8669e16c336737268176db
nguyet33 <tdnguyet33@gmail.com>
2023-03-27 16:34:43 -0700

    view role, department and employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,32 @@
-DROP DATABASE IF EXISTS atalla_corp_db;
-CREATE DATABASE atalla_corp_db;
-USE atalla_corp_db;
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
 
-  CREATE TABLE department(
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL
-  );
-  
-  CREATE TABLE roles (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(50),
-  salary DECIMAL(11, 2),
-  department_id INTEGER,
-  FOREIGN KEY(department_id) REFERENCES department(id)
- );
- 
-  CREATE TABLE employees (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  roles_id INTEGER NOT NULL,
-  manager_id INTEGER,
-  FOREIGN KEY (roles_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employees(id)
- );
+USE staff_db;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) 
+    ON DELETE CASCADE
+);
 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id), 
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
