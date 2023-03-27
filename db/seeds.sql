de6669be244c47555bfbfbb207a41b25d2706619
nguyet33 <tdnguyet33@gmail.com>
2023-03-27 14:41:24 -0700

    working seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,34 @@
-INSERT INTO departments (name)
-VALUES ("Finance"),
-        ("Service");
-       
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Accountant", 100000, 1),
-       ("Accountant Manager", 150000, 1),
-       ("Customer Service", 60000, 2),
-       ("Tax Specialist", 100000, 1),
-       ("Waiter", 40000, 2);
+ 
+ INSERT INTO department (name) VALUES
+  ("executive"),
+  ("sales"),
+  ("research"),
+  ("accounting"),
+  ("devops"),
+  ("compliance"),
+  ("hr");
 
-INSERT INTO employees(first_name, last_name, role_id, manager_id)
-VALUE ("Thien","Nguyen",2,null);
+ INSERT INTO roles (title, salary, department_id) VALUES
+  ("manager", 100000000, 1),
+  ("accountexec", 90000, 2),
+  ("scientist", 200000, 3),
+  ("accountant", 20000, 4),
+  ("engineer", 150000, 5),
+  ("attorney", 150000, 6),
+  ("recruiter", 40000, 7);
+ 
+  
+ INSERT INTO employees (first_name, last_name, roles_id, manager_id) VALUES
+  ("Thien", "Nguyen", 1, NULL),
+  ("Howard", "Lee", 2, NULL),
+  ("Jackie", "Lee", 3, NULL),
+  ("Andy", "Gaudy", 4, NULL),
+  ("Kim", "Nguyen", 5, NULL),
+  ("Max", "Adam", 6, NULL),
+  ("John", "Smith", 7, NULL);
 
+ 
+  
+
+
+  
